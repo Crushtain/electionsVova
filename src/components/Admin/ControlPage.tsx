98c93764baa75c7d32215342cdc0dfc0b8cb6a74
Kaydarov Pavel <psse4sspe@gmail.com>
2022-10-17 13:04:29 +0300

    admin table

--- src/components/Admin/ControlPage.tsx
+++ src/components/Admin/ControlPage.tsx
@@ -1,11 +1,39 @@
 import React from 'react';
 import {Button, Col, Collapse, Row, Typography} from "antd";
 import './styles.css';
+import Table from "./Table";
 
 const {Panel} = Collapse;
 const {Title} = Typography;
 
-const ControlPage = () => {
+interface DataType {
+    id: number;
+    name: string;
+    votesCount: number;
+    percent: number;
+}
+
+
+interface IProps {
+    data: DataType[];
+}
+
+const ControlPage = (props: IProps) => {
+    const { data } = props;
+    const isVotingStarted = false;
+
+    const loadData = () => {
+        // Запрос данных с апи
+    }
+
+    const startVoting = () => {
+        // Начать голосование
+    }
+
+    const stopVoting = () => {
+        // Закончить голосование
+    }
+
     return (
         <div className="admin-control-wrapper">
             <Row gutter={[24, 24]}>
@@ -26,25 +54,26 @@ const ControlPage = () => {
                 <Col span={24}>
                     <Collapse defaultActiveKey={1}>
                         <Panel key={1} header="Голосование">
-                            <Row justify="space-between">
+                            <Row>
                                 <Col>
-                                     <Button size="large" type="primary">
-                                        Запуск голосования
-                                    </Button>
-                                </Col>
-                                <Col>
-                                    <Button size="large" type="primary" danger>
-                                        Остановить голосование
-                                    </Button>
+                                    {isVotingStarted ? (
+                                        <Button size="large" type="primary" danger onClick={stopVoting}>
+                                            Остановить голосование
+                                        </Button>
+                                    ) : (
+                                        <Button size="large" type="primary" onClick={startVoting}>
+                                            Запуск голосования
+                                        </Button>
+                                    )}
                                 </Col>
                             </Row>
 
 
                         </Panel>
-                        <Panel key={2} header="Выгрузка">
-
+                        <Panel key={2} header="Промежуточные результаты" extra={<Button type="primary" onClick={loadData}>Загрузить</Button>}>
+                            <Table data={data}/>
                         </Panel>
-                        <Panel key={2} header="Лог выгрузок">
+                        <Panel key={2} header="Лог загрузок">
 
                         </Panel>
                     </Collapse>
